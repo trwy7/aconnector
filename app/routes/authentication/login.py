ec870fd31a7d6922b98e94a8957f27b6cfcbd384
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-04-09 22:12:07 -0600

    style: improved style

--- app/routes/authentication/login.py
+++ app/routes/authentication/login.py
@@ -1,7 +1,12 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from app.db import User
 
 @app.route("/login")
 def login_page():
     return render_template("auth/login.html")
+
+@app.route("/login", methods=['POST'])
+def login_post():
+    # TODO: Email regex
+    return str(request.form)
