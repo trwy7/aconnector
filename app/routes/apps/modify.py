76e632a6905906f632f393d32abfcb6cbdb85866
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-04-17 14:51:01 -0600

    misc: update restricted message

--- app/routes/apps/modify.py
+++ app/routes/apps/modify.py
@@ -7,7 +7,7 @@ from app.utilities.users import require_user
 @require_user
 def create_app_page():
     if request.user.disable_app_create:
-        return render_template("templates/error.html", status_code=403, error_message="Your account has been restricted from creating apps."), 403
+        return render_template("templates/error.html", status_code=403, error_message="An administrator has restricted your account from creating apps. Contact the site administrator for more information."), 403
     da = App.create(
         request.user,
         "My new app"
