da40c0c4648ab752c2d4dccf6f32e63aa6467bfa
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-04-17 09:54:02 -0600

    feat: split apps and modify page files

---
+++ app/routes/apps/modify.py
@@ -0,0 +1,43 @@
+from flask import render_template, request, redirect, abort
+from app import app
+from app.db import App
+from app.utilities.users import require_user
+
+@app.route("/apps/create")
+@require_user
+def create_app_page():
+    da = App.create(
+        request.user,
+        "My new app"
+    )
+    return redirect(f"/app/{da.client_id}")
+
+@app.route("/app/<string:clientid>")
+@require_user
+def modify_app_page(clientid):
+    ca = App.query.get(clientid)
+    if request.user.level == 4 or ca.owner == request.user:
+        return render_template("apps/modify.html", app=ca, host=request.host)
+    return abort(403)
+
+@app.route("/app/<string:clientid>", methods=["POST"])
+@require_user
+def modify_app(clientid):
+    ca = App.query.get(clientid)
+    if request.user.level == 4 or ca.owner == request.user:
+        ca.edit(
+            name=request.form['name'],
+            redirect_url=request.form['redir'],
+            launch_url=request.form['launch']
+        )
+        return redirect(request.path)
+    return abort(403)
+
+@app.route("/app/<string:clientid>/rerollsecret", methods=["POST"])
+@require_user
+def reroll_app_secret(clientid):
+    if ca.owner == request.user:
+        ca = App.query.get(clientid)
+        ca.reroll_secret()
+        return render_template("apps/reroll.html", app=ca)
+    return abort(403)
--- app/routes/errors.py
+++ app/routes/errors.py
@@ -6,6 +6,10 @@ from app import app
 def error_not_found(e):
     return render_template("templates/error.html", status_code=404, error_message="Not found"), 404
 
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return render_template("templates/error.html", status_code=405, error_message="Method not allowed"), 405
+
 @app.errorhandler(RateLimitExceeded)
 def ratelimited(e):   
     return render_template("templates/error.html", status_code=429, error_message="Too many requests"), 429
@@ -14,7 +18,3 @@ def ratelimited(e):
 @app.errorhandler(Exception)
 def internal_server_error(e):
     return render_template("templates/error.html", status_code=500, error_message="Internal server error"), 500
-
-@app.errorhandler(405)
-def method_not_allowed(e):
-    return render_template("templates/error.html", status_code=405, error_message="Method not allowed"), 405
