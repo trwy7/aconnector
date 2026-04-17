e46056f7c581d9b2312a578682daccf92ecbdf87
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-04-17 09:47:02 -0600

    feat: more error handler pages

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -57,7 +57,7 @@ def register_redir():
 
 @app.route("/register", methods=['POST'])
 @limiter.limit("2 per 2 seconds")
-@limiter.limit("15 per minute")
+@limiter.limit("5 per minute")
 @limiter.limit("30 per hour")
 def register_post():
     scode = ev_list.get(request.form['email'])
