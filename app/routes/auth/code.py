6f596c1b926bf617d306aa5590ec4ee769438944
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-04-17 18:41:12 -0600

    feat: login with code page

---
+++ app/routes/auth/code.py
@@ -0,0 +1,35 @@
+import random
+import re
+from datetime import datetime, timedelta
+from flask import render_template, request
+from app import app, logger, limiter
+from app.db import User
+
+code_list: dict[tuple[str, datetime], tuple[str, str]] = {}
+
+@app.route("/codelogin")
+def codelogin_page():
+    return render_template("auth/codelogin.html")
+
+@app.route("/codelogin", methods=['POST'])
+@limiter.limit("1 per 2 seconds", key_func=lambda: request.form.get("uname"))
+@limiter.limit("5 per minute", key_func=lambda: request.form.get("uname"))
+@limiter.limit("20 per hour", key_func=lambda: request.form.get("uname"))
+def codelogin_post():
+    rc = code_list.pop((request.form['code'], request.form['uname']), None)
+    if (not rc) or rc[1] < datetime.now():
+        return render_template("auth/codelogin.html", status="Invalid combination")
+    uid = rc[0]
+    luser = User.query.get(uid)
+    if not luser:
+        return "uh oh", 500
+    rs = render_template("auth/logincomplete.html", user=luser)
+    rs.set_cookie(
+        "abridgetoken",
+        luser.create_token().token,
+        httponly=True,
+        samesite="Lax",
+        secure=False, # Legacy requirement
+        max_age=2419200
+    )
+    return rs
