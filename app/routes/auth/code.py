dd9511931a13f9d144ad6fe25d706e8cac904084
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-04-17 19:46:45 -0600

    feat: working signin with another device

--- app/routes/auth/code.py
+++ app/routes/auth/code.py
@@ -1,9 +1,9 @@
 import random
-import re
 from datetime import datetime, timedelta
-from flask import render_template, request
-from app import app, logger, limiter
+from flask import render_template, request, make_response
+from app import app, limiter
 from app.db import User
+from app.utilities.users import require_user
 
 code_list: dict[tuple[str, datetime], tuple[str, str]] = {}
 
@@ -16,20 +16,28 @@ def codelogin_page():
 @limiter.limit("5 per minute", key_func=lambda: request.form.get("uname"))
 @limiter.limit("20 per hour", key_func=lambda: request.form.get("uname"))
 def codelogin_post():
-    rc = code_list.pop((request.form['code'], request.form['uname']), None)
+    rc = code_list.pop((int(request.form['code']), request.form['uname']), None)
     if (not rc) or rc[1] < datetime.now():
         return render_template("auth/codelogin.html", status="Invalid combination")
     uid = rc[0]
     luser = User.query.get(uid)
     if not luser:
         return "uh oh", 500
-    rs = render_template("auth/logincomplete.html", user=luser)
+    rs = make_response(render_template("auth/logincomplete.html", user=luser))
     rs.set_cookie(
         "abridgetoken",
         luser.create_token().token,
         httponly=True,
         samesite="Lax",
-        secure=False, # Legacy requirement
+        secure=False,
         max_age=2419200
     )
     return rs
+
+@app.route("/account/getlogincode", methods=['POST'])
+@limiter.limit("1 per 2 seconds", key_func=lambda: request.user)
+@require_user
+def codecreate_post():
+    code = random.randint(1000000, 9999999)
+    code_list[(code, request.user.username)] = (request.user.id, datetime.now() + timedelta(minutes=5))
+    return render_template("account/codegen.html", code=code)
