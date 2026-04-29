75c89f1ca7bd094046e7251f61c454c48db32478
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-04-29 16:56:23 -0600

    misc: zed

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
+import importlib
 import logging
 import os
 import re
-import importlib
-from flask import Flask, request, render_template
+
+from flask import Flask, render_template, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
@@ -13,25 +14,44 @@ logger = logging.getLogger("authbridge")
 # Init app
 logger.info("Starting AuthBridge")
 app = Flask(__name__, template_folder="pages", static_folder="static")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data/db.sqlite3'
-app.config['NAME'] = os.environ.get("APP_NAME", "AuthBridge")
-app.config['OWNER_EMAIL'] = os.environ.get("OWNER_EMAIL", "")
-app.config['CONTACT_EMAIL'] = os.environ.get("CONTACT_EMAIL", "")
-app.config['VEMAIL_REGEX'] = re.compile(os.environ.get("VEMAIL_REGEX", ""))
-app.config['VEMAIL_MESSAGE'] = os.environ.get("VEMAIL_MESSAGE", "")
-app.config['LOCK_NEW_USER_APP_CREATE'] = os.environ.get("LOCK_NEW_APP_CREATE", "true").lower() != "false"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////data/db.sqlite3"
+app.config["NAME"] = os.environ.get("APP_NAME", "AuthBridge")
+app.config["OWNER_EMAIL"] = os.environ.get("OWNER_EMAIL", "")
+app.config["CONTACT_EMAIL"] = os.environ.get("CONTACT_EMAIL", "")
+app.config["VEMAIL_REGEX"] = re.compile(os.environ.get("VEMAIL_REGEX", ""))
+app.config["VEMAIL_MESSAGE"] = os.environ.get("VEMAIL_MESSAGE", "")
+app.config["LOCK_NEW_USER_APP_CREATE"] = (
+    os.environ.get("LOCK_NEW_APP_CREATE", "true").lower() != "false"
+)
 
 from .utilities import users
+
+
 @app.before_request
 def auth():
     request.user, request.token = users.get_user()
-    if request.user and request.user.disabled and request.path not in ["/static/style.css", "/static/script.js"]:
-        logger.debug("[init/auth] Prevented disabled user %s from accessing %s", request.user.username, request.path)
+    if (
+        request.user
+        and request.user.disabled
+        and request.path not in ["/static/style.css", "/static/script.js"]
+    ):
+        logger.debug(
+            "[init/auth] Prevented disabled user %s from accessing %s",
+            request.user.username,
+            request.path,
+        )
         return render_template("account/disabled.html"), 403
+
 
 @app.context_processor
 def ctx_processor():
-    return {"gconfig": app.config, "user": request.user, "token": request.token, "host": request.host}
+    return {
+        "gconfig": app.config,
+        "user": request.user,
+        "token": request.token,
+        "host": request.host,
+    }
+
 
 ## Init extensions
 logger.debug("Init flask extensions")
@@ -41,7 +61,7 @@ limiter = Limiter(
     default_limits=["200 per 2 minutes", "20 per 10 seconds"],
     storage_uri="memory://",
     storage_options={"socket_connect_timeout": 30},
-    strategy="fixed-window"
+    strategy="fixed-window",
 )
 
 logger.debug("Init routes...")
@@ -50,9 +70,11 @@ rdir = os.path.join(os.path.dirname(__file__), "routes")
 for root, _, files in os.walk(rdir):
     for file in files:
         if file.endswith(".py"):
-            fd = os.path.join(root, file) \
-                .replace(rdir, "app.routes") \
-                .replace(os.sep, ".") \
+            fd = (
+                os.path.join(root, file)
+                .replace(rdir, "app.routes")
+                .replace(os.sep, ".")
                 .replace(".py", "")
+            )
             logger.debug("Init route %s", fd)
             importlib.import_module(fd)
