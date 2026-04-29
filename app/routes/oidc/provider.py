c85ac7907e63ccc57396efaf3d6fe73fd7b56950
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-04-29 12:49:41 -0600

    gh action test

--- app/routes/oidc/provider.py
+++ app/routes/oidc/provider.py
@@ -176,7 +176,6 @@ def auth_oidc_page():
         "exp": _now() + timedelta(seconds=AUTH_CODE_TTL_SECONDS),
     }
 
-    #db.session.commit()
     logger.info("[oidc] issued authorization code for user=%s client_id=%s", request.user.username, client.client_id)
     return redirect(f"{redirect_uri}?{urlencode({'code': code, 'state': state} if state else {'code': code})}")
 
