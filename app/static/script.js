fe798625c98fb7e537169bca85e5a30d5910b1f1
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-05-05 10:07:58 -0600

    fix: don't persist submitted class on page load

--- app/static/script.js
+++ app/static/script.js
@@ -5,3 +5,12 @@ forms.forEach(form => {
         form.classList.add("submitted")
     })
 })
+
+window.addEventListener('pageshow', function(event) {
+    if (event.persisted) {
+        const forms = document.querySelectorAll("form");
+        forms.forEach(form => {
+            form.classList.remove("submitted");
+        });
+    }
+});
